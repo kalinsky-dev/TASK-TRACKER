286f53f8995214d4ee591a64739a35db93fe0d28
Kalin Hristov <kalin0169@gmail.com>
2023-03-22 14:07:59 +0200

    update Controlled Form in Login Component

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,18 +3,31 @@ import { Link } from 'react-router-dom';
 
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPass] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPass] = useState('');
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: '',
+  })
+
+  const onChangeHandler = (e) => {
+    setFormValues(state => ({ ...state, [e.target.name]: e.target.value }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formValues);
+  };
 
 
 
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={onSubmit}>
       <div className="form-control">
         <label>Email:</label>
-        <input type="text" placeholder="Add Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="text" placeholder="Add Email" name="email" value={formValues.email} onChange={onChangeHandler} />
         <label>Password:</label>
-        <input type="text" placeholder="Add Password" value={password} onChange={(e) => setPass(e.target.value)} />
+        <input type="text" placeholder="Add Password" name="password" value={formValues.password} onChange={onChangeHandler} />
       </div>
       <input type="submit" className="btn" value="Log In" />
       <span>If you don't have a profile, click <Link to="/Register">here.</Link></span>
