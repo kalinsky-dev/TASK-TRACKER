712d88041685fc635afd039bfee2540d95a8ac03
Kalin Hristov <kalin0169@gmail.com>
2023-03-22 15:42:25 +0200

    Update ServerError.js

--- src/components/ServerError.js
+++ src/components/ServerError.js
@@ -7,7 +7,6 @@ const ServerError = () => {
       <h4>Something goes wrong...</h4>
       <h4>404 Not Found</h4>
       <h4>The requested item doesn't exist</h4>
-      <h4>The requested item doesn't exist</h4>
       <h4>Please try again!</h4>
       <Link to="/">Go Back</Link>
     </div>
