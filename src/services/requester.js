de994a0d569b94ebd6b61aa89a308bf77d2450f5
Kalin Hristov <kalin0169@gmail.com>
2023-03-23 16:08:44 +0200

    Update requester.js to make Authorized queries

--- src/services/requester.js
+++ src/services/requester.js
@@ -1,36 +1,39 @@
 const request = async (method, url, data) => {
-
-
   try {
+    const user = localStorage.getItem('auth');
+    const auth = JSON.parse(user || '{}');
+
+    let headers = {}
+
+    if (auth.accessToken) {
+      headers['X-Authorization'] = auth.accessToken;
+    }
 
     let buildRequest;
 
     if (method === 'GET') {
-      buildRequest = fetch(url);
+      buildRequest = fetch(url, { headers });
     } else {
       buildRequest = fetch(url, {
         method,
         headers: {
+          ...headers,
           'content-type': 'application/json'
         },
         body: JSON.stringify(data)
       });
-    };
-
+    }
     const response = await buildRequest;
 
     console.log(response);
-
 
     const result = await response.json();
 
     return result;
   } catch (error) {
-    // alert(error.message);
     console.log(error);
-    // throw error;
   }
-}
+};
 
 export const get = request.bind({}, 'GET');
 export const post = request.bind({}, 'POST');
