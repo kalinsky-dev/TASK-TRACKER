93e15b19608450eb14c65ad0a324125079774e12
Kalin Hristov <kalin0169@gmail.com>
2023-03-24 14:35:39 +0200

    update TaskService Component to send Create Query to the Server

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,6 +1,8 @@
 import * as request from "./requester";
 
-const baseUrl = 'http://localhost:3030';
+const baseUrl = 'http://localhost:3030/data/tasks';
 
-export const getAll = () => request.get(`${baseUrl}/data/tasks`);
+export const getAll = () => request.get(baseUrl);
+
+export const create = (gameData) => request.post(baseUrl, gameData);
 
