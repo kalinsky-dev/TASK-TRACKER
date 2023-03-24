bf24f0bffe42597d042b90798b8aa5517452ac40
Kalin Hristov <kalin0169@gmail.com>
2023-03-24 16:55:28 +0200

    Update Task Component
    Update the Task component to show the correct Owner of the Task

--- src/components/Task.js
+++ src/components/Task.js
@@ -8,6 +8,7 @@ const Task = ({
   hoursOfWork,
   takenByUser,
   inProgress,
+  owner,
   isFinished,
   _createdOn,
   _id,
@@ -27,7 +28,7 @@ const Task = ({
     <div className='task' onDoubleClick={() => onTaskClickHandler(_id)}>
       {isFinished ? <h3>{name} <FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => onDeleteClickHandler(_id)} /> </h3> : <h3>{name}</h3>}
       <h4>Description: {description}</h4>
-      <p>Task is created by: {_ownerId}</p>
+      <p>Task is created by: {owner}</p>
       {takenByUser ? <p>Task is taken by: {takenByUser}</p> : <p>This task is not taken yet!</p>}
       {inProgress && <p>Task is in progress!</p>}
       {isFinished && <p>Task is resolved for: {hoursOfWork} hours.</p>}
