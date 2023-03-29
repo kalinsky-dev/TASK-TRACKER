199e42b84187a1b5ede161f0317ba461280b45f6
Kalin Hristov <kalin0169@gmail.com>
2023-03-29 11:03:13 +0300

    Update About Component
    Go Back button link to different Components conditionally from the AuthState.

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,27 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+
+import { AuthContext } from "../contexts/AuthContext";
 
 
 const About = () => {
-  return (
-    <div>
-      <h4>Version 1.0.0</h4>
-      <Link to="/tasks">Go Back</Link>
-    </div>
-  );
+  const { user } = useContext(AuthContext);
+
+  if (user.email) {
+    return (
+      <div>
+        <h4>Version 1.0.0</h4>
+        <Link to="/tasks">Go Back</Link>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h4>Version 1.0.0</h4>
+        <Link to="/">Go Back</Link>
+      </div>
+    );
+  }
 };
 
 export default About;
