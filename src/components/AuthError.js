7ed81d26a72134cd86c85c89fe19c978ae4a99f2
Kalin Hristov <kalin0169@gmail.com>
2023-03-29 13:48:02 +0300

    Update Message

--- src/components/AuthError.js
+++ src/components/AuthError.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const AuthError = () => {
   return (
     <div>
-      <h4>Some of the fields are with incorrect or incomplete data.</h4>
+      <h4>Some of the fields you submitted are with incorrect or incomplete data.</h4>
       <h4>Please try again!</h4>
       <Link to="/register">Go Back</Link>
     </div>
