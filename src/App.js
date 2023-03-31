78be276735f60c8ed5db14571b4bb56dc15f6492
Kalin Hristov <kalin0169@gmail.com>
2023-03-31 17:49:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -49,44 +49,6 @@ function App() {
         });
   }, []);
 
-
-  // const [tasks, setTasks] = useState([
-  //   {
-  //     name: 'Bug_1',
-  //     description: 'Bug when we start the module',
-  //     hoursOfWork: null,
-  //     takenByUser: null,
-  //     inProgress: false,
-  //     isFinished: false,
-  //     _createdOn: '01.01.2022',
-  //     _id: '1',
-  //     _ownerId: 'Kalin'
-  //   },
-  //   {
-  //     name: 'Bug_2',
-  //     description: 'Bug with the onboarding emails',
-  //     hoursOfWork: 2,
-  //     takenByUser: 'Ivancho',
-  //     inProgress: false,
-  //     isFinished: true,
-  //     _createdOn: '02.01.2022',
-  //     _id: '2',
-  //     _ownerId: 'Kalin'
-  //   },
-  //   {
-  //     name: 'Bug_3',
-  //     description: 'Bug with the Contact Us Page',
-  //     hoursOfWork: null,
-  //     takenByUser: null,
-  //     inProgress: false,
-  //     isFinished: false,
-  //     _createdOn: '03.01.2022',
-  //     _id: '3',
-  //     _ownerId: 'Ivan'
-  //   },
-  // ]);
-
-
   const userLoginHandler = (authData) => {
     setAuth(authData)
   }
@@ -96,32 +58,16 @@ function App() {
   }
 
   const onTaskClickHandler = (taskId) => {
-    console.log(taskId);
+    // console.log(taskId);
     navigate(`/tasks/${taskId}`);
   };
 
 
-  // const onTakeItHandler = (taskId, e) => {
-  //   e.preventDefault();
-  //   console.log('TakeIt ', taskId);
-  //   // navigate(`/`);
-  // };
-
-  // const onFinishHandler = (taskId, e) => {
-  //   e.preventDefault();
-  //   console.log('Finish ', taskId);
-  //   // navigate(`/`);
-  // };
-
-
-
   const onDeleteClickHandler = (taskId, e) => {
     e?.preventDefault();
-    console.log(taskId);
+    // console.log(taskId);
     navigate(`/tasks/${taskId}/delete`);
   };
-
-
 
   const addTaskHandler = (taskData) => {
     setTasks(state => [...state, taskData]);
@@ -142,8 +88,6 @@ function App() {
     setTasks(sortedData);
     navigate('/tasks');
   };
-
-
 
   return (
     <AuthContext.Provider value={{ user: auth, userLoginHandler, userLogoutHandler }}>
