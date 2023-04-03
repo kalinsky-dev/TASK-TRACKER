bbae5c70c5b1bfea430daacd91469ef168a95429
Kalin Hristov <kalin0169@gmail.com>
2023-04-03 15:18:31 +0300

    Successful update of the requester to Throw  Errors when needed.

--- src/services/requester.js
+++ src/services/requester.js
@@ -1,37 +1,49 @@
 const request = async (method, url, data) => {
+
+  const user = localStorage.getItem('auth');
+
+  const auth = JSON.parse(user || '{}');
+
+  let headers = {}
+
+  if (auth.accessToken) {
+    headers['X-Authorization'] = auth.accessToken;
+  }
+
+  let buildRequest;
+
+  if (method === 'GET') {
+    buildRequest = fetch(url, { headers });
+  } else {
+    buildRequest = fetch(url, {
+      method,
+      headers: {
+        ...headers,
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+  }
+
   try {
-    const user = localStorage.getItem('auth');
-    const auth = JSON.parse(user || '{}');
-
-    let headers = {}
-
-    if (auth.accessToken) {
-      headers['X-Authorization'] = auth.accessToken;
-    }
-
-    let buildRequest;
-
-    if (method === 'GET') {
-      buildRequest = fetch(url, { headers });
-    } else {
-      buildRequest = fetch(url, {
-        method,
-        headers: {
-          ...headers,
-          'content-type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
-    }
     const response = await buildRequest;
-
     // console.log(response);
+    // console.log(response.status);
 
-    const result = await response.json();
-
-    return result;
+    if (response.ok !== true) {
+      const error = await response.json();
+      // console.log(error);
+      throw new Error(error.message)
+    } else {
+      const result = await response.json();
+      return result;
+    }
+    // console.log(response);
+    // const result = await response.json();
+    // return result;
   } catch (error) {
-    console.log(error);
+    // console.log(error);
+    throw error;
   }
 };
 
@@ -40,3 +52,62 @@ export const post = request.bind({}, 'POST');
 export const patch = request.bind({}, 'PATCH');
 export const put = request.bind({}, 'PUT');
 export const del = request.bind({}, 'DELETE');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const request = async (method, url, data) => {
+//   try {
+//     const user = localStorage.getItem('auth');
+//     const auth = JSON.parse(user || '{}');
+
+//     let headers = {}
+
+//     if (auth.accessToken) {
+//       headers['X-Authorization'] = auth.accessToken;
+//     }
+
+//     let buildRequest;
+
+//     if (method === 'GET') {
+//       buildRequest = fetch(url, { headers });
+//     } else {
+//       buildRequest = fetch(url, {
+//         method,
+//         headers: {
+//           ...headers,
+//           'content-type': 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//       });
+//     }
+
+//     const response = await buildRequest;
+
+//     // console.log(response);
+
+//     const result = await response.json();
+
+//     return result;
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// };
+
+// export const get = request.bind({}, 'GET');
+// export const post = request.bind({}, 'POST');
+// export const patch = request.bind({}, 'PATCH');
+// export const put = request.bind({}, 'PUT');
+// export const del = request.bind({}, 'DELETE');
+
