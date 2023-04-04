2de3824a5a686c132e3f9b15c75cf0aff3df351e
Kalin Hristov <kalin0169@gmail.com>
2023-04-04 19:39:49 +0300

    Update TaskItem.test.js

--- src/components/TaskItem.test.js
+++ src/components/TaskItem.test.js
@@ -1,6 +1,5 @@
 import { cleanup, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-
 import { BrowserRouter } from 'react-router-dom';
 
 import { AuthContext } from '../contexts/AuthContext';
