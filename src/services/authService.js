6e808256c5a737cd3892f9d37ccc6b6af9ba106f
Kalin Hristov <kalin0169@gmail.com>
2023-04-05 14:47:24 +0300

    Prepare authService.js for Deploy

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,24 +1,27 @@
 import * as request from "./requester";
 
-const baseUrl = 'http://localhost:3030/users';
+const baseUrl = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:3030'
+  : 'http://localhost:3030'; // TODO: Add server url when deployed
+
+const url = `${baseUrl}/users`;
 
 export const login = ({ email, password }) =>
-  request.post(`${baseUrl}/login`, { email, password });
+  request.post(`${url}/login`, { email, password });
 
 
 export const logout = async (accessToken) => {
   try {
-    const response = await fetch(`${baseUrl}/logout`, {
+    const response = await fetch(`${url}/logout`, {
       headers: {
         'X-Authorization': accessToken
       }
     });
     return response;
   } catch (error) {
-    // console.log(error);
     throw error;
   }
 };
 
 export const register = ({ email, password }) =>
-  request.post(`${baseUrl}/register`, { email, password });
+  request.post(`${url}/register`, { email, password });
