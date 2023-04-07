f390a6bd28bbc0c2c04dc7efcf76f24edf932b42
Kalin Hristov <kalin0169@gmail.com>
2023-04-07 14:28:51 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@
 const Home = () => {
   return (
     <div>
-      <h4>Welcome in the site, where you and your team can store and manage the Tasks for the day.</h4>
+      <h4>Welcome in the site, where you and your team can store and manage the Tasks for your business.</h4>
       <h4>Please make you registration or Login.</h4>
       <h4>Enjoy your work!</h4>
     </div>
